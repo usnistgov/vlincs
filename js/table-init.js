b2b04cfebcdf2fe3532f53fc5d680f7f2d9afe40
Tim Blattner <timothy.blattner@nist.gov>
2024-12-11 21:51:37 -0500

    Actor update 20241212T025137

--- js/table-init.js
+++ js/table-init.js
@@ -5,18 +5,16 @@ var sort_col;
 
 if ($('#testing-nov2024-test-results').find("th:contains('IDF1')").length > 0)
 {  sort_col = $('#testing-nov2024-test-results').find("th:contains('IDF1')")[0].cellIndex;
-  $('[data-toggle="tooltip"]').tooltip();
   $('#testing-nov2024-test-results').dataTable({ order: [[ sort_col, 'desc' ]] });
 }
 if ($('#testing-nov2024-test-all-results').find("th:contains('IDF1')").length > 0)
 {  sort_col = $('#testing-nov2024-test-all-results').find("th:contains('IDF1')")[0].cellIndex;
-  $('[data-toggle="tooltip"]').tooltip();
   $('#testing-nov2024-test-all-results').dataTable({ order: [[ sort_col, 'desc' ]] });
 }
 if ($('#testing-nov2024-test-jobs').find("th:contains('Submission Timestamp')").length > 0)
 {  sort_col = $('#testing-nov2024-test-jobs').find("th:contains('Submission Timestamp')")[0].cellIndex;
-  $('[data-toggle="tooltip"]').tooltip();
   $('#testing-nov2024-test-jobs').dataTable({ order: [[ sort_col, 'desc' ]] });
 }
+  $('[data-toggle="tooltip"]').tooltip();
 $('.dataTables_length').addClass('bs-select');
 });
